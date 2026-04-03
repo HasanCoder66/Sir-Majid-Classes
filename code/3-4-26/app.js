f64b098f1dfdd344df89409d1074825274b2aefa
hasanCoder66 <hasanashraf874@gmail.com>
2026-04-03 17:57:32 +0500

    arrow fn

--- code/3-4-26/app.js
+++ code/3-4-26/app.js
@@ -44,13 +44,38 @@ let arr = [1,2,3,4,5]
 // arguement wahan hota hai jahan fn call kia jata hai 
 
 
-let filterItems = arr.filter(function (item){
-if(item > 3){
-//    return item
-}
-})
+// let filterItems = arr.filter(function (item){
+// if(item > 3){
+// //    return item
+// }
+// })
 
-console.log(filterItems, "Filter ==>")
+// console.log(filterItems, "Filter ==>")
 
-const filteredItems = arr.filter((item) => item > 3)
-console.log(filteredItems,  "Filtered ==>")
+// const filteredItems = arr.filter((item) => item > 3)
+// console.log(filteredItems,  "Filtered ==>")
+
+
+
+// arrow fn
+
+// function normalFn (){
+// console.log(arguments)
+// }
+
+// normalFn(12,34,56,78)
+
+
+
+// const arrowFn = (...a) => {
+//     return a
+// }
+
+
+// console.log(arrowFn(12,34,56,78))
+
+
+const oneLineFn = (...a) => a
+
+
+console.log(oneLineFn(12,1,2,5,7))
